4d717e158458287d6b61ac31a8d03b3000ed0aa5
m3k4cyber <moraesariel607@gmail.com>
2024-01-12 17:24:40 -0300

    DScareca

---
+++ slá/model/DB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE livro (
+  isbn VARCHAR(16), 
+  titulo VARCHAR(30), 
+  altor_do_livro VARCHAR(30), 
+  sexo_do_altor CHAR, 
+  qtd_páginas VARCHAR(4), 
+  editora VARCHAR(20), 
+  estado_da_publicação VARCHAR(8), 
+  valor_de_venda BIGINT)
