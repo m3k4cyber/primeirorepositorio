3aeee605d0feaef30a36ddeb2f560f681e2ff4ab
m3k4cyber <moraesariel607@gmail.com>
2024-01-18 17:17:09 -0300

    DScarecaa

--- Dark_Souls_Guia/codigos_gerais/log.java
+++ Dark_Souls_Guia/codigos_gerais/log.java
@@ -1,3 +1,6 @@
-function redirecionar() {
-    window.location.href = "../mainP.html";
+
+function toggleTable(id) {
+    var table = document.getElementById(id + '-table');
+    table.style.display = (table.style.display == 'none' || table.style.display == '') ? 'block' : 'none';
 }
+
